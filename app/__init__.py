a8334ce712a8acd0e1ecd5a417305a4ceb6bb6a7
monge <ojdupool2004@mail.ru>
2025-05-16 15:45:08 +0300

    release app directory

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(config_class=None):
+    app = Flask(__name__)
+    app.config.from_object(config_class or 'config.Config')
+
+    db.init_app(app)
+
+    from .routes import bp as api_bp
+    app.register_blueprint(api_bp)
+
+    return app
