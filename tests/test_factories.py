4b6cbd21b96a189c7fab5a4b9afb4492a743c57f
monge <ojdupool2004@mail.ru>
2025-05-16 15:58:37 +0300

    release тесты directory

---
+++ tests/test_factories.py
@@ -0,0 +1,14 @@
+from factories.factories import ClientFactory, ParkingFactory
+from app import db
+
+def test_create_client_with_factory(app):
+    with app.app_context():
+        client = ClientFactory()
+        db.session.commit()
+        assert client.id is not None
+
+def test_create_parking_with_factory(app):
+    with app.app_context():
+        parking = ParkingFactory()
+        db.session.commit()
+        assert parking.count_places == parking.count_available_places
