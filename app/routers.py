2fd355d32723d72a6569230c676e307155f320e5
monge <ojdupool2004@mail.ru>
2025-05-19 15:00:26 +0300

    ьнзн

--- app/routers.py
+++ app/routers.py
@@ -10,7 +10,7 @@ bp = Blueprint("api", __name__)
 
 
 @bp.route("/clients", methods=["POST"])
-def create_client() -> FlaskResponse:
+def create_client() -> FlaskResponse | tuple[FlaskResponse, int]:
     data: Dict[str, Any] = request.get_json()
     name: str = data["name"]
     client = Client(name=name)
@@ -20,13 +20,13 @@ def create_client() -> FlaskResponse:
 
 
 @bp.route("/clients/<int:client_id>", methods=["GET"])
-def get_client(client_id: int) -> FlaskResponse:
+def get_client(client_id: int) -> FlaskResponse | tuple[FlaskResponse, int]:
     client = Client.query.get_or_404(client_id)
     return jsonify({"id": client.id, "name": client.name})
 
 
 @bp.route("/parkings", methods=["POST"])
-def create_parking() -> FlaskResponse:
+def create_parking() -> FlaskResponse | tuple[FlaskResponse, int]:
     data: Dict[str, Any] = request.get_json()
     location: str = data["location"]
     parking = Parking(location=location)
@@ -36,13 +36,13 @@ def create_parking() -> FlaskResponse:
 
 
 @bp.route("/parkings/<int:parking_id>", methods=["GET"])
-def get_parking(parking_id: int) -> FlaskResponse:
+def get_parking(parking_id: int) -> FlaskResponse | tuple[FlaskResponse, int]:
     parking = Parking.query.get_or_404(parking_id)
     return jsonify({"id": parking.id, "location": parking.location})
 
 
 @bp.route("/assign", methods=["POST"])
-def assign_client_to_parking() -> FlaskResponse:
+def assign_client_to_parking() -> FlaskResponse | tuple[FlaskResponse, int]:
     data: Dict[str, Any] = request.get_json()
     client_id: int = data["client_id"]
     parking_id: int = data["parking_id"]
